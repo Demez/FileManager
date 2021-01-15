b1563e3cc2e347a8464f801417f7c38a941c303c
Demez <demizexp@gmail.com>
2021-01-15 10:52:30 -0500

    fixed main

--- src/demez_file_manager_qt5.cpp
+++ src/demez_file_manager_qt5.cpp
@@ -423,7 +423,7 @@ FileItem::~FileItem()
 // ================================================================
 
 
-int main(int argc, char* argv[])
+auto main(int argc, char* argv[]) -> int
 {
     QApplication app(argc, argv);
     // QGuiApplication appGUI(argc, argv);
